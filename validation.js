ee237c076ba586db514f22ef64c731d283af6ebf
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-03 17:06:21 +0100

    Final commit with form

--- validation.js
+++ validation.js
@@ -1,1 +1,20 @@
-// validation script here
+const inputs = document.querySelectorAll("input");
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/,
+  password: /^[\w@-]{8,20}$/,
+  slug: /^[a-z\d-]{8-20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+function validate(field, regex) {
+  regex.test(field.value)
+    ? (field.className = "valid")
+    : (field.className = "invalid");
+}
+
+inputs.forEach(input => {
+  input.addEventListener("keyup", e => {
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
